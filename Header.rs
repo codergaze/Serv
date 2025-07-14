5c0e4f03c2a300f3c243e2561fc639dbce6e18c0
codergaze <kolagundaharshavardhan@gmail.com>
2025-07-14 23:21:16 +0530

    Add reserved later

--- Header.rs
+++ Header.rs
@@ -13,3 +13,28 @@ pub struck Dnsheader {
     pub au_count : u16;
     pub r_entry : u16;
 }
+impl Dnsheader {
+    pub fn new() -> Dnsheader{
+        Dnsheader{
+            id : 0;
+            qr : false;
+            opcode : false;
+            AA : false;
+            TC : false;
+            RD : false;
+            RA : false;
+            rcode : 0;
+            qucount : 0;
+            acount : 0;
+            au_count : 0;
+            r_entry : 0;
+        }
+    }
+    pub fn reader (&mut self , buffer: &mut BytePacketBuffer) -> Result<()>{
+        self.id = buffer.read_u16()?;
+        let flag = buffer.read_u16()?;
+        let a = (flag >> 8) as u8;
+        let b = (flags & 0xFF) as u8;
+        self.
+
+}
